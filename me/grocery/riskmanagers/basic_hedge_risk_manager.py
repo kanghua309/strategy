6c3a01f2fcef5b1b10bebdd31fb389d276a7198d
kanghua309 <kanghua151@gmail.com>
2017-10-06 17:14:19 +0800

    ....

--- me/grocery/riskmanagers/basic_hedge_risk_manager.py
+++ me/grocery/riskmanagers/basic_hedge_risk_manager.py
@@ -6,16 +6,16 @@ from .riskmanager import RiskManager
 import cvxpy as cvx
 
 MAX_GROSS_LEVERAGE = 1.0
-NUM_LONG_POSITIONS = 19 #剔除risk_benchmark
-NUM_SHORT_POSITIONS = 0
-MAX_BETA_EXPOSURE = 0.50
+#NUM_LONG_POSITIONS  = 20 #剔除risk_benchmark
+#NUM_SHORT_POSITIONS = 0
+MAX_BETA_EXPOSURE   = 2.00 #0.5
 
-NUM_ALL_CANDIDATE = NUM_LONG_POSITIONS
+#NUM_ALL_CANDIDATE = NUM_LONG_POSITIONS
 
-MAX_LONG_POSITION_SIZE = 2 * 1.0/(NUM_LONG_POSITIONS + NUM_SHORT_POSITIONS)
+#MAX_LONG_POSITION_SIZE = 2 * 1.0/(NUM_LONG_POSITIONS + NUM_SHORT_POSITIONS)
 #MAX_SHORT_POSITION_SIZE = 2*1.0/(NUM_LONG_POSITIONS + NUM_SHORT_POSITIONS)
 
-MIN_LONG_POSITION_SIZE = 0.5 * 1.0/(NUM_LONG_POSITIONS + NUM_SHORT_POSITIONS)
+#MIN_LONG_POSITION_SIZE = 0.5 * 1.0/(NUM_LONG_POSITIONS + NUM_SHORT_POSITIONS)
 MAX_SECTOR_EXPOSURE = 0.3
 
 
@@ -29,13 +29,15 @@ class BasicHedgeRiskManager(RiskManager):
     def optimalize(self,candidates,factors):
         print ("Optimalize - candidates:",len(candidates),candidates)
 
-
         candidates_len = candidates.index
         w = cvx.Variable(len(candidates_len))
         # objective = cvx.Maximize(df.pred.as_matrix() * w)  # mini????
         objective = cvx.Maximize(candidates[factors['ALPHA']].as_matrix() * w)           #FIX IT
         constraints = [cvx.sum_entries(w) == 1.0 * MAX_GROSS_LEVERAGE, w >= 0.0]  # dollar-neutral long/short
         # constraints.append(cvx.sum_entries(cvx.abs(w)) <= 1)  # leverage constraint
+        MAX_LONG_POSITION_SIZE = 2 * 1.0 / (candidates_len)
+        MIN_LONG_POSITION_SIZE = 0.5 * 1.0 / (candidates_len)
+
         constraints.extend([w >= MIN_LONG_POSITION_SIZE, w <= MAX_LONG_POSITION_SIZE])  # long exposure
         riskvec = candidates[factors['BETA']].fillna(1.0).as_matrix()             # TODO
         constraints.extend([riskvec * w <= MAX_BETA_EXPOSURE])                    # risk ?
