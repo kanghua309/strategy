fb4f04ecae696ac56dd449eb318b4a44f74a41bc
kanghua <kanghua151@gmail.com>
2017-12-01 16:01:03 +0800

    Merge branch 'master' of https://github.com/kanghua309/strategy
    # Conflicts:
    #	campaign/deeplearn/training.py
    #	me/helper/research_env.py

--- campaign/checkmode/factors.py
+++ campaign/checkmode/factors.py
@@ -11,21 +11,45 @@ from zipline.pipeline.data import USEquityPricing
 from zipline.pipeline.factors import CustomFactor, Returns, Latest
 from me.pipeline.factors.tsfactor import Fundamental
 
+import talib
 
 class ILLIQ(CustomFactor):
     inputs = [USEquityPricing.close,USEquityPricing.volume]
     window_length = int(252)
     def compute(self, today, assets, out, close,volume):
-        _rets = pd.DataFrame(close, columns=assets).pct_change()[1:]
-        print(_rets.head(10))
+        window_length = len(close)
+        #print "window length",window_length
+        _rets = np.abs(pd.DataFrame(close, columns=assets).pct_change()[1:])
+        #print(_rets.head(10))
         _vols  = pd.DataFrame(volume, columns= assets)[1:]
-        print(_vols.head(10))
-        print "--------------------------1"
-        print(_rets/_vols)
-        print "--------------------------2"
+        #print(_vols.head(10))
+        #print "--------------------------1"
+        #print(_rets/_vols)
+        #print "--------------------------2"
+        #print pd.rolling_mean(_rets/_vols, window=window_length-1)
+        #print (_rets/_vols).mean(),type((_rets/_vols).mean())
+        out[:] =(_rets/_vols).mean().values
 
-        print pd.rolling_mean(_rets/_vols, window=2)
-        #out[:] =
+
+class MOM(CustomFactor):
+    # this class generates the MACD as a Percentage
+    inputs = [USEquityPricing.close]
+    window_length = int(252)
+
+    def columnwise_anynan(self,array2d):
+        return np.isnan(array2d).any(axis=0)
+    def compute(self, today, assets, out, close):
+            window_length= len(close)
+            print window_length,np.shape(close)
+            anynan = self.columnwise_anynan(close)
+            for col_ix, have_nans in enumerate(anynan):
+                if have_nans:
+                    out[col_ix] = np.nan
+                    continue
+                mom = talib.MOM(close[:, col_ix],timeperiod=window_length)
+                print "---mom :", mom
+                out[col_ix] = mom[-1]
+
 
 
 import click
@@ -34,7 +58,7 @@ import pandas as pd
 from zipline.pipeline import Pipeline
 from zipline.pipeline.data import USEquityPricing
 from zipline.pipeline.factors import Latest
-from zipline.pipeline.factors import Returns
+from zipline.pipeline.factors import Returns,AverageDollarVolume
 from zipline.utils.cli import Date
 
 from me.helper.research_env import Research
@@ -60,7 +84,14 @@ def make_pipeline(asset_finder):
     # returns = Returns(inputs=[USEquityPricing.close], window_length=5)  # 预测一周数据
     private_universe = private_universe_mask(['000001','000002'],asset_finder=asset_finder)
 
-    illiq = ILLIQ(window_length=10,mask = private_universe)
+    illiq = ILLIQ(window_length=20,mask = private_universe)
+    ep = 1/Fundamental(asset_finder).pe
+    bp = 1/Fundamental(asset_finder).pb
+    bvps = Fundamental(asset_finder).bvps
+    rev20 = Returns(inputs=[USEquityPricing.close], window_length=20)
+    vol20 = AverageDollarVolume(window_length=20)
+    mom = MOM(window_length=20)
+
     pipe_columns = {
         # 'h2o': h2o.log1p().zscore(),
         # 'l2o': l2o.log1p().zscore(),
@@ -72,6 +103,9 @@ def make_pipeline(asset_finder):
         # 'turnover_rate': turnover_rate.log1p().zscore(),
         # 'return': returns.log1p(),
         'ILLIQ':illiq,
+        'ep':ep,
+        'vol20':vol20,
+        'mom':mom,
     }
     # pipe_screen = (low_returns | high_returns)
     pipe = Pipeline(columns=pipe_columns,screen=private_universe)
@@ -81,8 +115,9 @@ def make_pipeline(asset_finder):
 
 pd.set_option('display.width', 800)
 research = Research()
-print(research.get_engine()._finder)
+#print(research.get_engine()._finder)
 my_pipe = make_pipeline(research.get_engine()._finder)
 result = research.run_pipeline(my_pipe,
                                Date(tz='utc', as_timestamp=True).parser(start),
                                Date(tz='utc', as_timestamp=True).parser(end))
+print result
