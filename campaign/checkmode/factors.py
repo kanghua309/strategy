5f423e61050dcf815e107450769579658e081ff9
kanghua <kanghua151@gmail.com>
2017-12-03 20:02:45 +0800

    Merge branch 'master' of https://github.com/kanghua309/strategy
    # Conflicts:
    #	campaign/deeplearn/training.py
    #	me/helper/research_env.py

--- campaign/checkmode/factors.py
+++ campaign/checkmode/factors.py
@@ -16,16 +16,9 @@ from zipline.pipeline.classifiers import CustomClassifier,Latest
 from me.pipeline.factors.boost import Momentum
 import tushare as ts
 
-import talib
-
-import click
-import numpy as np
-import pandas as pd
 from zipline.pipeline import Pipeline
 from zipline.pipeline.data import USEquityPricing
-from zipline.pipeline.factors import Latest
 from zipline.pipeline.factors import Returns,AverageDollarVolume
-from zipline.utils.cli import Date
 
 from me.helper.research_env import Research
 from me.pipeline.factors.tsfactor import Fundamental
@@ -33,15 +26,15 @@ from me.pipeline.filters.universe import make_china_equity_universe, default_chi
     private_universe_mask
 from zipline.utils.cli import Date, Timestamp
 
-start = '2015-8-5'  # 必须在国内交易日
-end   = '2015-9-28'  # 必须在国内交易日
+start = '2017-8-10'   # 必须在国内交易日
+end   = '2017-9-11'  # 必须在国内交易日
 
 c,_ = get_sector_class()
 ONEHOTCLASS = tuple(c)
 
 hs300 = ts.get_hs300s()['code']
-print type(hs300),hs300
-print hs300.tolist()
+#print type(hs300),hs300
+#print hs300.tolist()
 
 
 class ILLIQ(CustomFactor):
@@ -54,14 +47,6 @@ class ILLIQ(CustomFactor):
         out[:] =(_rets/_vols).mean().values
 
 def make_pipeline(asset_finder):
-    # universe = make_china_equity_universe(
-    #     target_size=300,
-    #     #mask=default_china_equity_universe_mask(['000001']),
-    #     mask=None,
-    #     max_group_weight=0.01,
-    #     smoothing_func=lambda f: f.downsample('week_start'),
-    #     asset_finder = asset_finder,
-    # )
 
     private_universe = private_universe_mask( hs300.tolist(),asset_finder=asset_finder)
     #print private_universe_mask(['000001','000002','000005'],asset_finder=asset_finder)
@@ -97,8 +82,9 @@ def make_pipeline(asset_finder):
         'ILLIQ':illiq.zscore(groupby=sector,mask=illiq.percentile_between(1, 99)),
         'mom'  :mom.zscore(groupby=sector,mask=mom.percentile_between(1, 99)),
         'rsi'  :rsi.zscore(groupby=sector,mask=rsi.percentile_between(1, 99)),
-        'sector':sector,
-        'returns':returns.quantiles(100),
+        #'sector':sector,
+        #'returns':returns.quantiles(100),
+        'returns': returns,
 
     }
     # pipe_screen = (low_returns | high_returns)
@@ -112,7 +98,6 @@ def make_pipeline(asset_finder):
     return pipe
 
 
-
 pd.set_option('display.width', 8000)
 research = Research()
 #print(research.get_engine()._finder)
@@ -121,12 +106,13 @@ result = research.run_pipeline(my_pipe,
                                Date(tz='utc', as_timestamp=True).parser(start),
                                Date(tz='utc', as_timestamp=True).parser(end))
 
-print result
-print type(result)
-print result.reset_index()
+print result.head(10)
+#print type(result)
+#print result.reset_index()
 #result.replace([np.inf,-np.inf],np.nan)
 result = result.reset_index().drop(['level_0','level_1'],axis = 1).replace([np.inf,-np.inf],np.nan).fillna(0)
-
+print "#####################################"
+print result.head(10)
 # print result.isnull().any()
 # print result[result.isnull().values==True]
 
@@ -141,11 +127,47 @@ print "============================="
 # imp = Imputer(missing_values='NaN', strategy='mean', axis=0)
 # imp.fit(X)
 
-quadratic_featurizer  = PolynomialFeatures(interaction_only=True)
-X_train_quadratic = quadratic_featurizer.fit_transform(result)
-print X_train_quadratic
-print np.shape(X_train_quadratic),type(X_train_quadratic)
+# quadratic_featurizer  = PolynomialFeatures(interaction_only=True)
+# X_train_quadratic = quadratic_featurizer.fit_transform(result)
+# print X_train_quadratic
+# print np.shape(X_train_quadratic),type(X_train_quadratic)
+#
+# print quadratic_featurizer.get_feature_names(result.columns),len(quadratic_featurizer.get_feature_names(result.columns))
 
-print quadratic_featurizer.get_feature_names(result.columns),len(quadratic_featurizer.get_feature_names(result.columns))
+
+
+
+
+X = result.drop('returns', 1)
+Y = result['returns']
+
+test_size=100
+Train_X = X[:-test_size]
+Train_Y = Y[:-test_size]
+Test_X  = X[-test_size:]
+Test_Y  = Y[-test_size:]
+
+print ("*******************************************")
+print Test_Y.head(10),len(Test_Y)
+print Test_X.head(10),len(Test_X)
+#
+#
+# TRAIN_X = X[0:]
+# TRAIN_Y = array[0:400,13]
+#
+# TEST_X = array[400:,0:13]
+# TEST_Y = array[400:,13]
+
+
+print X.head(10)
+print Y.head(10)
+from modeltest import model_cross_valid,model_fit_and_test
+
+model_cross_valid(Train_X.values,Train_X.values)
+print ("---------------fit and test")
+model_fit_and_test(Train_X.values,Train_Y.values,Test_X.values,Test_Y.values)
+
+
 
 #print type(result.as_matrix()),result.as_matrix
+
--- campaign/checkmode/modeltest.py
+++ campaign/checkmode/modeltest.py
@@ -11,24 +11,46 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.ensemble import AdaBoostRegressor
 from sklearn.ensemble import GradientBoostingRegressor
 
+#
+# url = "https://archive.ics.uci.edu/ml/machine-learning-databases/housing/housing.data"
+# names = ['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD', 'TAX', 'PTRATIO', 'B', 'LSTAT', 'MEDV']
+# dataframe = pandas.read_csv(url, delim_whitespace=True, names=names)
+# array = dataframe.values
+# X = array[:,0:13]
+# Y = array[:,13]
+# print dataframe.head(10)
 
-url = "https://archive.ics.uci.edu/ml/machine-learning-databases/housing/housing.data"
-names = ['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD', 'TAX', 'PTRATIO', 'B', 'LSTAT', 'MEDV']
-dataframe = pandas.read_csv(url, delim_whitespace=True, names=names)
-array = dataframe.values
-X = array[:,0:13]
-Y = array[:,13]
-print dataframe.head(10)
 
-seed = 7
-kfold = model_selection.KFold(n_splits=10, random_state=seed)
-def bulid_model(model_name):
-    model = model_name()
-    return model
-scoring = 'neg_mean_squared_error'
-# + random fest boost lstm gbdt
 
-for model_name in [LinearRegression,Ridge,Lasso,ElasticNet,KNeighborsRegressor,DecisionTreeRegressor,SVR,RandomForestRegressor,AdaBoostRegressor,GradientBoostingRegressor]:
-    model = bulid_model(model_name)
-    results = model_selection.cross_val_score(model, X, Y, cv=kfold, scoring=scoring)
-    print(model_name,results.mean())
+def model_cross_valid(X,Y):
+    seed = 7
+    kfold = model_selection.KFold(n_splits=10, random_state=seed)
+    def bulid_model(model_name):
+        model = model_name()
+        return model
+    scoring = 'neg_mean_squared_error'
+    # + random fest boost lstm gbdt
+
+    for model_name in [LinearRegression,Ridge,Lasso,ElasticNet,KNeighborsRegressor,DecisionTreeRegressor,SVR,RandomForestRegressor,AdaBoostRegressor,GradientBoostingRegressor]:
+        model = bulid_model(model_name)
+        results = model_selection.cross_val_score(model, X, Y, cv=kfold, scoring=scoring)
+        print(model_name,results.mean())
+
+import numpy as np
+def model_fit_and_test(TrainX,TrainY,TestX,TestY):
+    def bulid_model(model_name):
+        model = model_name()
+        return model
+
+    for model_name in [LinearRegression, Ridge, Lasso, ElasticNet, KNeighborsRegressor, DecisionTreeRegressor, SVR,
+                       RandomForestRegressor, AdaBoostRegressor, GradientBoostingRegressor]:
+        model = bulid_model(model_name)
+        model.fit(TrainX,TrainY)
+        print("====================",model_name)
+        print("Residual sum of squares: %.2f"% np.mean((model.predict(TestX) - TestY) ** 2))
+        print model.predict(TestX)
+        print TestY
+        # Explained variance score: 1 is perfect prediction
+        print('Variance score: %.2f' % model.score(TestX, TestY))
+
+# model_cross_valid(X,Y)
